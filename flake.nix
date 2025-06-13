d7259f58125e6751ccbf922fbaaf023c49dd1d5d
Alexandros Liarokapis <liarokapis.v@gmail.com>
2025-06-13 21:34:54 +0300

    add flake files

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({
+      systems = [
+        "x86_64-linux"
+      ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          devShells.default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.nodePackages.nodejs
+              pkgs.nodePackages.typescript-language-server
+              pkgs.tailwindcss-language-server
+              pkgs.svelte-language-server
+            ];
+          };
+        };
+    });
+}
