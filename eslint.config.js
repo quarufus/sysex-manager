6782657f0759349a943c959fab45ba8bbaa39422
Alexandros Liarokapis <liarokapis.v@gmail.com>
2025-06-13 21:37:10 +0300

    properly enable eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,42 @@
-import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
-import { includeIgnoreFile } from '@eslint/compat';
+import ts from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
-import { fileURLToPath } from 'node:url';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
 
-const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
-
-export default ts.config(
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: {
-			globals: { ...globals.browser, ...globals.node }
+const tsConfig = ts.config({
+	files: ['**/*.js', '**/*.ts', '**/*.svelte'],
+	extends: [
+		js.configs.recommended,
+		...ts.configs.strictTypeChecked,
+		...ts.configs.stylisticTypeChecked,
+		prettier
+	],
+	languageOptions: {
+		globals: {
+			...globals.browser,
+			...globals.nodeBuiltin
 		},
-		rules: { 'no-undef': 'off' }
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
-			}
+		parserOptions: {
+			projectService: true,
+			tsconfigRootDir: import.meta.dirname,
+			extraFileExtensions: ['.svelte'],
+			programs: false
 		}
 	}
-);
+});
+
+const svelteConfig = ts.config({
+	files: ['**/*.svelte'],
+	extends: [...svelte.configs['flat/recommended'], ...svelte.configs['flat/prettier']],
+	languageOptions: {
+		parserOptions: {
+			projectService: true,
+			parser: ts.parser
+		}
+	}
+});
+
+export default ts.config(...tsConfig, ...svelteConfig, {
+	ignores: ['eslint.config.js', 'svelte.config.js', 'build', '.svelte-kit/']
+});
